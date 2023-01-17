38157e3e3189be8f65409a4b0a463d1969c4bcfa
hucik14 <hucik14@gmail.com>
2023-01-17 16:10:45 +0100

    Update urdf_testing.py

--- roboticstoolbox/robot/urdf_testing.py
+++ roboticstoolbox/robot/urdf_testing.py
@@ -6,7 +6,15 @@ from roboticstoolbox.models.ETS import Panda, Puma560
 r = ERobot(ET.Rz() * ET.tx(1) * ET.tz(2) * ET.Rz() * ET.tx(1))
 q = [3, -2]
 
+
 p = Panda()
 p.generate_URDF()
 q = [0, 0, 0, 0, 0, 0, 0]
 print(p.fkine(q))
+
+
+# Init joint to the 'ready' joint angles
+p.q = p.qr
+
+# Open a plot with the teach panel
+e = p.teach()
